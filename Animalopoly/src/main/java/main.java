e7ef0697a39d26944c5621d70c0aa6e6e9080faa
Colcothar <33038729+Colcothar@users.noreply.github.com>
2020-03-17 17:12:44 +0000

    Update main.java

--- Animalopoly/src/main/java/main.java
+++ Animalopoly/src/main/java/main.java
@@ -11,29 +11,26 @@ import java.util.*;
  * @author Admin
  */
 class main {    
-    public static void main(String[] args) {            
+    public static void main(String[] args) {    
+        
         String animals[]={"0","Dog","Cat","Fish","Parrot","Cow","Goat","Sheep","Chicken","Pig","Geese","Fox","Dingo","0","Octopus","Doplhin","Whale","Narwhale","Flamingo", "Panda","Rhino","Giraffe","Cheetah","Elephant","Tiger","lion"};
         int cost[]={0,20,30,50,60,100,120,140,200,250,260,300,350,0,390,410,460,500,510,560,600,640,670,700,800,850} ;   
-        int num;
+        int numOfPlayers;
 
-        Scanner reader = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);       
+     
         
         ArrayList<Animals> animal = new ArrayList<>();
+        ArrayList<Player> player = new ArrayList<>();
+        
         for(int x=0; x<26; x++ ){
              animal.add( new Animals(animals[x],cost[x],0));
         }
-        //System.out.println(animal.get(25).getAnimal());
         
+        Game game = new Game(animal, player);
+        numOfPlayers = game.numOfPlayer();
         
-        do{ 
-            System.out.println("How many players, max of 8, min of 4?");
-            num = Integer.parseInt(reader.nextLine());
-        }while ((num<4)||(num>8));
-        
-        
-   
-       
-
+        for(int x=0; x< numOfPlayers;x++){
+            player.add( new Player(x));
         }
-}
-
+        
