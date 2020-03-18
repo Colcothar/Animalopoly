ecd7f1be55eedb3fd2f14069f39f2015438884ca
AlexM1010 <37385715+AlexM1010@users.noreply.github.com>
2020-03-18 21:57:39 +0000

    Working condition
    Not sure how reliable it is but seems to be working from my testing

--- Animalopoly/src/main/java/Cards.java
+++ Animalopoly/src/main/java/Cards.java
@@ -1,123 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import static java.lang.Math.random;
 import java.util.*;
-
-/**
- *
- * @author Admin
- */
 
  public class Cards {
     
-
-
     private ArrayList<String> list = new ArrayList<>();
-    private ArrayList<Integer> list2 = new ArrayList<>();
-    int num = 0;
-    Random random = new Random();
-
-    
+    int cardVal[] = {50,-100,-150,-140,75,150,100,60,-100,-100,100,100,-69,-200,200,-50,11,-200,-100,100};
     
     public Cards(){
+        //Add cards to array
         list.add("Bank pays you dividend");
-
         list.add("Pay speeding fine");
-
         list.add("Pay parking fine ");
-
         list.add("Pay general repirs");
-
         list.add("You have been elected Chairman of the Board");
-
         list.add("Your building and loan matures");
-
         list.add("You have won a crossword competition");
-
         list.add("You have won a beauty competition");
-
         list.add("Drunk driving" );
-
         list.add("Pay CFD overnight charges");
-
         list.add("The stock market booms");
-
         list.add("Receive a mysterious cash prize");
-
         list.add("Pay library fines");
-
         list.add("You’ve been caught avoiding tax" );
-
         list.add("you’ve won the lottery");
-
         list.add("You’ve got ransomware");
-
         list.add("Netflix Premium Subscription");
-
         list.add("Brexit fees");
-
-
         list.add("Accidental Car damage");
-
         list.add("Pay rise ");
-
-
-        list2.add(50);
-
-        list2.add(-100);
-
-        list2.add(-150);
-
-        list2.add(-140);
-
-        list2.add(75);
-
-        list2.add(150);
-
-        list2.add(100);
-
-        list2.add(60);
-
-        list2.add(-100);
-
-        list2.add(-100);
-
-        list2.add(100);
-
-        list2.add(100);
-
-        list2.add(69);
-
-        list2.add(-200);
-
-        list2.add(200);
-
-        list2.add(-50);
-
-        list2.add(11);
-
-
-        list2.add(-200);
-
-        list2.add(-100);
-
-        list2.add(100);
     }
     
-    public void getCard(){
-        num = random.nextInt(list.size());
-        System.out.println(list.get(num) );
+    public String getCard(int num1){
+        return list.get(num1);
     }
     
-    public int getCardAmount(){
-       return list2.get( num );
+    public int getCardAmount(int num2){
+       return cardVal[num2];
     }
     
-    
-
-
 }
--- Animalopoly/src/main/java/Player.java
+++ Animalopoly/src/main/java/Player.java
@@ -1,9 +1,5 @@
 import java.util.Scanner; 
 
-/**
- * @author alexm
- */
-
 public class Player {
     //Initialise variables
     private final String name, piece;
@@ -20,49 +16,28 @@ public class Player {
         
         //Take input from the player
         Scanner s = new Scanner(System.in);  // Create a Scanner object (s)
-        System.out.println("Enter name");
+        System.out.println("Enter your name");
         name = s.nextLine(); //scan input 
         System.out.println("Enter a charachter to use as a piece");
         piece = s.nextLine(); //scan input 
-
     }        
     
     //Misc Functions
     public void adjustMoney(double amount){money += amount;} //Add and remove money (negative val for remove)
     
     //Getters
-    public int getMoney(){
-        return money;
-    }
-    public String getName(){
-        return name;
-    }
-    public String getPiece(){
-        return piece;
-    }
-    public int getPlayerID(){
-        return playerID;
-    }
-    public int getPosition(){
-        return position;
-    }
-    public int getMissTurn(){
-        return missTurn;
-    }
-    public int getBankrupt(){
-        return bankrupt;
-    }
+    public int getMoney(){return money;}
+    public String getName(){return name;}
+    public String getPiece(){return piece;}
+    public int getPlayerID(){return playerID;}
+    public int getPosition(){return position;}
+    public int getMissTurn(){return missTurn;}
+    public int getBankrupt(){return bankrupt;}
     
     //Setters  
-    public void setPosition(int pos){
-        position = pos;
-    }
-    public void setMissTurn(){
-        missTurn = 2;
-    }
-    public void setBankrupt(){
-        bankrupt = 0;}
-    
+    public void setPosition(int pos){position = pos;}
+    public void setMissTurn(){missTurn = 2;}
+    public void setBankrupt(){bankrupt = 0;}
     public void missTurn(){
         if(missTurn>0){
             missTurn--;
